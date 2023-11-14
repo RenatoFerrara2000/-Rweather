d978e9af545ddc17028eb551605f5b4a5fd47d02
RenatoFerrara2000 <reny.2000@hotmail.it>
2023-11-14 15:18:30 +0100

    Initial Commit

---
+++ ЯRweather/_RweatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  _RweatherApp.swift
+//  ЯRweather
+//
+//  Created by Renato Ferrara on 14/11/23.
+//
+
+import SwiftUI
+
+@main
+struct _RweatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
